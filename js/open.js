836bff3583286429ea079521bd2b017c77331957
이은정 <donnie.lee@pospot.kr>
2022-04-21 18:21:03 +0900

    html site

---
+++ js/open.js
@@ -0,0 +1,35 @@
+function apply_modal() {
+    console.log("")
+    $(".modal_wrapper").css({
+        "display":"flex"
+    });
+};
+
+function close_modal() {
+    console.log("1")
+    $(".modal_wrapper").css({
+        "display":"none"
+    });
+};
+
+function open_post() {
+    console.log("12")
+    $(".post_modal_wrapper").css({
+        "display":"flex"
+    });
+}
+
+function close_post() {
+    console.log("123")
+    $(".post_modal_wrapper").css({
+        "display":"none"
+    });
+}
+
+function ir_coming(){
+    console.log("1234")
+    $(".ir").css({
+        "display":"block"
+    });
+}
+
