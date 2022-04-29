7d8cae139a4e8ecc998569c9690bd9b0ef1dd542
choiseungnam <ckbird@pospot.kr>
2022-04-29 21:18:08 +0900

    frontend firstupload

---
+++ pospot_final/src/App.js
@@ -0,0 +1,41 @@
+/* eslint-disable */
+
+import './App.css';
+import React, { Component, useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+/* CSS */
+import './resources/css/common.css';
+import './resources/css/contents.css';
+
+/* Component */
+import Header from './component/Header';
+import Footer from './component/Footer';
+import Main from './component/Main';
+import Log from './component/Log';
+import Log2 from './component/Log2';
+import RecruitList from './component/RecruitList';
+import RecruitView from './component/RecruitView';
+import Contact from './component/Contact';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <Header/>
+      <div className="Content">
+        <Routes>
+          <Route path='/' element={<Main/>}></Route>
+          <Route path='/log' element={<Log/>}></Route>
+          <Route path='/recruit/list' element={<RecruitList/>}></Route>
+          <Route path='/recruit/detail' element={<RecruitView/>}></Route>
+          <Route path='/contact' element={<Contact/>}></Route>
+        </Routes>
+      </div>
+      <Footer/>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
